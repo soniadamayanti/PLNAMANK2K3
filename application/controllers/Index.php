eaeda93beb957ee8745d727fd687880b25b05865
rizkihardinas <36495798+rizkihardinas@users.noreply.github.com>
2019-03-15 09:34:10 +0700

    Add Insert

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -18,7 +18,8 @@ class Index extends CI_Controller
 		$this->load->view('parts/footer');
 	}
 	function sop(){
-		$data['judul'] = "Home";
+		$data['judul'] = "SOP";
+		$data['pelaksana'] = $this->database_model->get('tb_pelaksana');
 		$this->load->view('parts/header');
 		$this->load->view('parts/menu');
 		$this->load->view('sop_view', $data);
@@ -72,6 +73,40 @@ class Index extends CI_Controller
 	
 		$kode = ".".sprintf("%03s", $max[0]['kode'])."/AMANK2K3/CIANJUR/".$bulan."/".$tahun;
 		echo $kode;
+	}
+	function insert_sop(){
+		$kode_project = $this->input->post('kode_project');
+		$kode_project = date('Y-m-d H:i:s');
+		$kode_user = $this->session->userdata('kode_user');
+		$tegangan = $this->input->post('tegangan');
+		$material = $this->input->post('material');
+		$jml_tenaga_kerja = $this->input->post('jml_tenaga_kerja');
+		$peralatan_kerja = $this->input->post('peralatan_kerja');
+		$kode_project = $this->input->post('kode_project');
+
+	}
+	function insert_temp_uraian_pekerjaan(){
+		$uraian_perkerjaan = $this->input->post('uraian_perkerjaan');
+		$jam = $this->input->post('jam');
+		$keterangan = $this->input->post('keterangan');
+		$kode_project = $this->input->post('kode_project');
+		$data_uraian = array(
+			'kode_uraian_pekerjaan' => '',
+			'uraian_perkerjaan' => $uraian_perkerjaan,
+			'jam' => $jam,
+			'keterangan' => $keterangan,
+			'kode_project' => $kode_project
+		);
+		if (is_null($kode_project)) {
+			echo "Kode project masih kosong,reload kembali";
+		}else if(is_null($uraian_perkerjaan) || is_null($jam)){
+			echo "Uraian pekerjaan dan waktu tidak boleh kosong";
+		}else{
+			$this->database_model->insert('tb_temp_uraian_pekerjaan',$data_uraian);
+		}
+	}
+	function insert_temp_pelaksana(){
+		
 	}
 }
 
