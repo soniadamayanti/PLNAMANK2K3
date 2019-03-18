948aea1a3684fe0feac54d1b405eaafbfaede8ec
rizkihardinas <36495798+rizkihardinas@users.noreply.github.com>
2019-03-18 13:47:44 +0700

    Update db_project_pln.sql

--- db_project_pln.sql
+++ db_project_pln.sql
@@ -6,7 +6,6 @@
 -- Generation Time: Mar 17, 2019 at 07:12 PM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 7.1.7
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
